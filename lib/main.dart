5f28b26918d6ca56db3324e404311d2d9a58f353
albanysiswanto14 <albanysiswanto14@gmail.com>
2024-09-30 05:16:44 +0000

    first

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'pekot',
+      home: Scaffold(
+        body: Center(
+          child: Text('Pekot Demo'),
+        ),
+      )
+    );
+  }
+}
